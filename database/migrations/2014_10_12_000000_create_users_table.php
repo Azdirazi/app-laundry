58ee77f637c78de21fa6a04601fdbf310b3b224b
Azdirazi <91419014+Azdirazi@users.noreply.github.com>
2023-07-17 20:01:07 +0700

    tabelcostumer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username', 100);
             $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 150);
+            $table->string('gender', 15);
+            $table->longText('address');
+            $table->string('phone', 20);
+            $table->string('');
+            $table->string('level');
             $table->timestamps();
         });
     }
